3f997824e853979309fc82991868f0d1f94601a4
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2025-04-08 08:21:05 -0300

    Adicionado anotação @Configuration

---
+++ src/main/java/br/com/jfood/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package br.com.jfood.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/users/register").hasRole("MANAGER")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(resourceServer ->
+                        resourceServer.jwt(jwt -> {
+                        })
+                );
+
+        return http.build();
+    }
+}
