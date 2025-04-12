04b0ef3410e596d025088d7c8e4b5cde323dafd8
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2025-04-12 15:59:08 -0300

    Atualizado arquivo migration

--- src/main/resources/db/migration/V1__create_table_users.sql
+++ src/main/resources/db/migration/V1__create_table_users.sql
@@ -5,6 +5,7 @@ CREATE TABLE users (
     username VARCHAR(100) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     phone VARCHAR(20) NOT NULL,
-    address VARCHAR(100) NOT NULL
+    address VARCHAR(100) NOT NULL,
+    role VARCHAR(50) NOT NULL
 );
 
