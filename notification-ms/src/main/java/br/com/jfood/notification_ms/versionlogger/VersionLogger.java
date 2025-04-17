fd143ac82f93d5938c72e7976870ab1ff4ed9cd6
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2025-04-16 22:33:48 -0300

    Atualizado versionlogger e arquivo de properties

--- notification-ms/src/main/java/br/com/jfood/notification_ms/versionlogger/VersionLogger.java
+++ notification-ms/src/main/java/br/com/jfood/notification_ms/versionlogger/VersionLogger.java
@@ -1,6 +1,8 @@
 package br.com.jfood.notification_ms.versionlogger;
 
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class VersionLogger {
+
+    private static final Logger logger = LoggerFactory.getLogger(VersionLogger.class);
 
     /**
      * It logs the current application version.
@@ -18,10 +22,9 @@ public class VersionLogger {
     @PostConstruct
     private void postConstruct() {
         final String CURRENT_NUMBER_VERSION = "1.0.0";
-        final String CURRENT_DATE_VERSION = "15-Apr-2025";
+        final String CURRENT_DATE_VERSION = "16-Apr-2025";
 
-        System.out.println("JFOOD - NOTIFICATION-MS - STARTING - Version: "
-                + CURRENT_NUMBER_VERSION + " - " + CURRENT_DATE_VERSION);
+        logger.info("JFOOD - NOTIFICATION-MS - STARTING - Version: {} - {}", CURRENT_NUMBER_VERSION, CURRENT_DATE_VERSION);
     }
 
 }
